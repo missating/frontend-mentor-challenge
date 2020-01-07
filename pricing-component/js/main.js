6f3541ee0759961646c84f3cd2c7943d15fbc2a4
missating <atingenkay@gmail.com>
2020-01-07 01:36:33 +0100

    add script for price change when toggled

---
+++ pricing-component/js/main.js
@@ -0,0 +1,13 @@
+const checkbox = document.querySelector('.toggle');
+const priceMonth = document.querySelectorAll('.price-month');
+const priceYear = document.querySelectorAll('.price-year');
+
+checkbox.addEventListener('change', () => {
+	if (checkbox.checked) {
+		priceYear.forEach(price => price.classList.add('hidden'));
+		priceMonth.forEach(price => price.classList.remove('hidden'));
+	} else {
+		priceYear.forEach(price => price.classList.remove('hidden'));
+		priceMonth.forEach(price => price.classList.add('hidden'));
+	}
+});
