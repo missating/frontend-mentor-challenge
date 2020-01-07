f78f4072bbc7bcfdfba0042dc8ffee12ef566c0f
missating <atingenkay@gmail.com>
2020-01-07 03:47:05 +0100

    add link to code pen

--- pricing-component/README.md
+++ pricing-component/README.md
@@ -1,3 +1,7 @@
 # Frontend Mentor - File Transfer pricing component
 
 ![Design preview for the File Transfer pricing component coding challenge](./design/desktop-preview.jpg)
+
+# View On Codepen
+
+https://codepen.io/missating/full/rNapPgm
--- pricing-component/js/main.js
+++ pricing-component/js/main.js
@@ -3,11 +3,11 @@ const priceMonth = document.querySelectorAll('.price-month');
 const priceYear = document.querySelectorAll('.price-year');
 
 checkbox.addEventListener('change', () => {
-	if (checkbox.checked) {
-		priceYear.forEach(price => price.classList.add('hidden'));
-		priceMonth.forEach(price => price.classList.remove('hidden'));
-	} else {
-		priceYear.forEach(price => price.classList.remove('hidden'));
-		priceMonth.forEach(price => price.classList.add('hidden'));
-	}
+  if (checkbox.checked) {
+      priceYear.forEach(price => price.classList.add('hidden'));
+      priceMonth.forEach(price => price.classList.remove('hidden'));
+  } else {
+      priceYear.forEach(price => price.classList.remove('hidden'));
+      priceMonth.forEach(price => price.classList.add('hidden'));
+  }
 });
