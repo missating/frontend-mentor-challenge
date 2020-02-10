b5413783c20c4f66e75b84c52a10da3c1602a43a
missating <atingenkay@gmail.com>
2020-02-10 04:25:57 +0100

    set up intro section on desktop and mobile

--- insure-landing-page/js/main.js
+++ insure-landing-page/js/main.js
@@ -1,23 +1,23 @@
-const navHamburgerIcon = document.querySelector(".header__hamburger");
-const nav = document.querySelector(".header__nav");
-const navItems = document.querySelector(".header__nav__items");
+// const navHamburgerIcon = document.querySelector(".header__hamburger");
+// const nav = document.querySelector(".header__nav");
+// const navItems = document.querySelector(".header__nav__items");
 
-navHamburgerIcon.addEventListener("click", () => {
-  if (nav.classList.contains("nav-open")) {
-    closeNav();
-  } else {
-    openNav();
-  }
-});
+// navHamburgerIcon.addEventListener("click", () => {
+//   if (nav.classList.contains("nav-open")) {
+//     closeNav();
+//   } else {
+//     openNav();
+//   }
+// });
 
-const closeNav = () => {
-  navHamburgerIcon.src = "./images/icon-hamburger.svg";
-  nav.classList.remove("nav-open");
-  nav.style.visibility = "hidden";
-};
+// const closeNav = () => {
+//   navHamburgerIcon.src = "./images/icon-hamburger.svg";
+//   nav.classList.remove("nav-open");
+//   nav.style.visibility = "hidden";
+// };
 
-const openNav = () => {
-  navHamburgerIcon.src = "./images/icon-close.svg";
-  nav.classList.add("nav-open");
-  nav.style.visibility = "visible";
-};
+// const openNav = () => {
+//   navHamburgerIcon.src = "./images/icon-close.svg";
+//   nav.classList.add("nav-open");
+//   nav.style.visibility = "visible";
+// };
