38c0026a4b15b8592833c2d62a3f24f3589f0800
missating <atingenkay@gmail.com>
2020-02-12 03:00:19 +0100

    add media queries for responsiveness

--- insure-landing-page/js/main.js
+++ insure-landing-page/js/main.js
@@ -1,23 +1,36 @@
-// const navHamburgerIcon = document.querySelector(".header__hamburger");
-// const nav = document.querySelector(".header__nav");
-// const navItems = document.querySelector(".header__nav__items");
+const navHamburgerIcon = document.querySelector(".header__hamburger");
+const nav = document.querySelector(".header__nav");
+const navItems = document.querySelector(".header__nav__items");
 
-// navHamburgerIcon.addEventListener("click", () => {
-//   if (nav.classList.contains("nav-open")) {
-//     closeNav();
-//   } else {
-//     openNav();
-//   }
-// });
+navHamburgerIcon.addEventListener("click", () => {
+  if (nav.classList.contains("nav-open")) {
+    closeNav();
+  } else {
+    openNav();
+  }
+});
 
-// const closeNav = () => {
-//   navHamburgerIcon.src = "./images/icon-hamburger.svg";
-//   nav.classList.remove("nav-open");
-//   nav.style.visibility = "hidden";
-// };
+const closeNav = () => {
+  navHamburgerIcon.src = "./images/icon-hamburger.svg";
+  nav.classList.remove("nav-open");
+  nav.setAttribute("aria-modal", true);
+  navHamburgerIcon.setAttribute("aria-expanded", false);
+  navHamburgerIcon.focus();
+  nav.style.display = "none";
+};
 
-// const openNav = () => {
-//   navHamburgerIcon.src = "./images/icon-close.svg";
-//   nav.classList.add("nav-open");
-//   nav.style.visibility = "visible";
-// };
+const openNav = () => {
+  navHamburgerIcon.src = "./images/icon-close.svg";
+  nav.classList.add("nav-open");
+  navHamburgerIcon.setAttribute("aria-expanded", true);
+  navItems.firstElementChild.firstElementChild.focus();
+  document.addEventListener("keyup", navKeyStrokeClose, event);
+  nav.style.display = "block";
+};
+
+const navKeyStrokeClose = event => {
+  if (event.keyCode === 27) {
+    closeNav();
+    document.removeEventListener("keyup", navKeyStrokeClose);
+  }
+};
