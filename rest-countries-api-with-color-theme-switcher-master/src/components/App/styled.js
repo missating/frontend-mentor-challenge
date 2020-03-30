d69f20b8d0ae214f7239fe1321d1df1998782bb9
missating <atingenkay@gmail.com>
2020-03-30 18:47:26 +0100

    add configuration for css

--- rest-countries-api-with-color-theme-switcher-master/src/components/App/styled.js
+++ rest-countries-api-with-color-theme-switcher-master/src/components/App/styled.js
@@ -1,8 +1,3 @@
 import styled from "styled-components";
 
-export const AppContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: red;
-`;
+export const AppContainer = styled.div``;
--- rest-countries-api-with-color-theme-switcher-master/webpack.config.js
+++ rest-countries-api-with-color-theme-switcher-master/webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         ]
       },
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
         resolve: {
           alias: {
             components: path.resolve(__dirname, "src/components/"),
